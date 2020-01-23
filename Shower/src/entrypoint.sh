820b56e664e95087a8f35f4f62de2868aa17353f
Mirza Merdovic <mirzamerdovic@gmail.com>
2020-01-23 23:26:12 +0100

    Changes: - Dockerfile refactoring - Playlist reading - Added shower ASCII art outro

--- Shower/src/entrypoint.sh
+++ Shower/src/entrypoint.sh
@@ -2,9 +2,29 @@
 
 main() {
 	echo "Starting... \n"
-    echo ${pwd}
+    echo "Work directory: $PWD"
+	
+	input="shower/playlist"
+	
+	echo "Playlist content:"
+	while read line
+	do
+		echo "\t $line"
+	done < "$input"
+
+	outro
 	
 	sleep infinity
+}
+
+outro() {
+	echo "\t\t" '   .--./""---.._             '
+	echo "\t\t" '  /`.-.|      //\\           '
+	echo "\t\t" '   /   \     /::|| `--.      '
+	echo "\t\t" '        `.  ||::// ` `--.    '
+	echo "\t\t" '          `.\\://  `-. `-.   '
+	echo "\t\t" '            '--' `. `.       '
+	echo "\t\t" '                '.   .  '-.  '
 }
 
 main
